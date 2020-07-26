5932d8410df804d37dce0482a1a76d16eb857e09
AmeyKhoje <ameykhoje@gmail.com>
2020-07-26 12:42:26 +0530

    adding store results on click immutability using concat

--- src/UseReducer/UseReducer1.js
+++ src/UseReducer/UseReducer1.js
@@ -7,6 +7,7 @@ const UseReducer1 = (props) => {
     const [count, dispatch] = useReducer(reducer, initialState);
     return (
         <div>
+            <hr />
             {count}
             <button
                 onClick = {() => dispatch('INC')}>
@@ -30,6 +31,21 @@ const UseReducer1 = (props) => {
             <div>
                 <button onClick={props.onAdd}>Add 10 using redux</button>
             </div>
+            <hr />
+            <div>
+                <button onClick={props.onStoreResult}>Store Result</button>
+            </div>
+            <div>
+                <ul>
+                    {
+                        props.storedResult.map(str => (
+                            <li onClick={props.onDeleteResult} key={str.id}>{str.value}</li>
+                        ))
+                    }
+                    
+                </ul>
+            </div>
+            <hr />
         </div>
     );
 }
@@ -40,14 +56,17 @@ const mapStateToProps = state => {  //Maps the redux state to props of component
     return {
         ctr: state.counter,
         name: state.name,
-        surname: state.surname
+        surname: state.surname,
+        storedResult: state.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onAdd: () => dispatch({ type: 'ADD', value: 10 })
+        onAdd: () => dispatch({ type: 'ADD', value: 10 }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' })
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,8 @@
 const initialState = {
     counter: 0,
     name: '',
-    surname: ''
+    surname: '',
+    results: []
 }
 
 const reducer = ( state = initialState, action ) => {
@@ -18,6 +19,11 @@ const reducer = ( state = initialState, action ) => {
                 ...state,
                 counter: state.counter + action.value,
                 name: state.name = 'Amey'
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(),value:state.counter})
             }
     }
     return state;
